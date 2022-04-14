12d52c18ddc590339fc2a2b8d20b7635ac995a23
Stepan <st.luka2001@gmail.com>
2022-04-14 18:34:07 +0200

    third practic lesson

---
+++ js/script.js
@@ -0,0 +1,99 @@
+/* Задание на урок:
+
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
+
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами*/
+
+'use strict';
+
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt("Сколько фильмов ты посмотрел?", "");
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt("Сколько фильмов ты посмотрел?", "");
+    }
+}
+
+start();
+
+
+const personalMovieDB = {
+    count : numberOfFilms,
+    movies : {},
+    actors : {},
+    genres : [],
+    privat : false
+};
+
+
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const   film = prompt('Один из последних просмотренных фильмов?', ""),
+                rateOfFilm = prompt("На сколько оцените его?", "");
+    
+        if(film != null && rateOfFilm != null && film != '' && rateOfFilm != '' && film.length < 50) {
+            personalMovieDB.movies[film] = rateOfFilm;
+            console.log("done");
+        } else {
+            console.log("error");
+            i--;
+        }
+    } 
+}
+
+rememberMyFilms();
+
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 0){
+        console.log("Произошла ошибка");
+    } else if(personalMovieDB.count < 10){
+        console.log("Просмотрено довольно мало фильмов");
+    } else if(personalMovieDB.count <= 30 && personalMovieDB.count >= 10){
+        console.log("Вы классический зритель");
+    } else if(personalMovieDB.count > 30) {
+        console.log("Вы киноман");
+    }  else {
+        console.log("Произошла ошибка");
+    }
+}
+
+detectPersonalLevel();
+
+
+function showMyDB() {
+    if(personalMovieDB.privat === false) {
+        console.log(personalMovieDB);
+    }
+    // return personalMovieDB;
+}
+
+showMyDB();
+
+
+function writeYourGenres() {
+    for(let i = 1; i <= 3; i++) {
+        const favoriteGenre = prompt(`Ваш любимый жанр под номером ${i}`);
+        if(favoriteGenre != null && favoriteGenre != '' && favoriteGenre.length < 50) {
+        personalMovieDB.genres.push(favoriteGenre);
+        } else {
+            i--;
+        }
+    }
+}
+
+writeYourGenres();
+
+
+
+
